754e6911be9a76b6cd38ffe8d9bcf7a54d91f7a8
Iamneo <techadmin@iamneo.ai>
2023-09-06 12:05:10 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,49 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Here, you can add your search logic using the 'searchText' state.
+    // For now, let's just display an alert with the search text.
+    alert(`Searching for: ${searchText}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +51,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +74,4 @@ function App() {
 }
 
 export default App;
+
